2649ab90fd94f02c6326f7b7c2acb8f48bc05d2d
KarinA <abdullaeva.karina.r@gmail.com>
2022-12-23 20:20:51 +0300

    Refactor parsing.py

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -8,7 +8,11 @@ def get_normal_value(file, key):
 def parse(file1, file2):
     keys1 = set(file1)
     keys2 = set(file2)
+
     all_keys = keys1 | keys2
+    deleted = keys1 - keys2
+    remaining = keys1 & keys2
+    added = keys2 - keys1
 
     lines = []
 
@@ -16,15 +20,15 @@ def parse(file1, file2):
         value1 = get_normal_value(file1, key)
         value2 = get_normal_value(file2, key)
 
-        if key in (keys1 - keys2):
+        if key in deleted:
             lines.append(f'  - {key}: {value1}')
-        elif key in (keys1 & keys2):
+        elif key in remaining:
             if value1 == value2:
                 lines.append(f'    {key}: {value1}')
             else:
                 lines.append(f'  - {key}: {value1}')
                 lines.append(f'  + {key}: {value2}')
-        elif key in (keys2 - keys1):
+        elif key in added:
             lines.append(f'  + {key}: {value2}')
 
     return lines
