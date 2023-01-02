d276d0555a7ba56119d6db3b2740bbebfd3fa293
KarinA <abdullaeva.karina.r@gmail.com>
2023-01-02 13:03:38 +0300

    Refactor test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,37 +1,28 @@
+import pytest
 from gendiff import generate_diff
 
 
-def test_generate_diff_json():
-    file_path1 = './tests/fixtures/file1.json'
-    file_path2 = './tests/fixtures/file2.json'
-    result = generate_diff(file_path1, file_path2)
-    correct = open('./tests/fixtures/result.txt')
-
-    assert result == correct.read()
+JSON1 = './tests/fixtures/file1.json'
+JSON2 = './tests/fixtures/file2.json'
+YAML1 = './tests/fixtures/file1.yml'
+YAML2 = './tests/fixtures/file2.yml'
+RESULT = './tests/fixtures/result.txt'
 
 
-def test_generate_diff_yaml():
-    file_path1 = './tests/fixtures/file1.yml'
-    file_path2 = './tests/fixtures/file2.yml'
-    result = generate_diff(file_path1, file_path2)
-    correct = open('./tests/fixtures/result.txt')
-
-    assert result == correct.read()
+JSON1_N = './tests/fixtures/file1_nested.json'
+JSON2_N = './tests/fixtures/file2_nested.json'
+YAML1_N = './tests/fixtures/file1_nested.yml'
+YAML2_N = './tests/fixtures/file2_nested.yml'
+RESULT_N = './tests/fixtures/result_nested.txt'
 
 
-def test_generate_diff_nested_json():
-    file_path1 = './tests/fixtures/file1_nested.json'
-    file_path2 = './tests/fixtures/file2_nested.json'
-    result = generate_diff(file_path1, file_path2)
-    correct = open('./tests/fixtures/result_nested.txt')
-
-    assert result == correct.read()
-
-
-def test_generate_diff_nested_yaml():
-    file_path1 = './tests/fixtures/file1_nested.yml'
-    file_path2 = './tests/fixtures/file2_nested.yml'
-    result = generate_diff(file_path1, file_path2)
-    correct = open('./tests/fixtures/result_nested.txt')
-
+@pytest.mark.parametrize('input1, input2, expected', [
+    (JSON1, JSON2, RESULT),
+    (YAML1, YAML2, RESULT),
+    (JSON1_N, JSON2_N, RESULT_N),
+    (YAML1_N, YAML2_N, RESULT_N)
+])
+def test_generate_diff(input1, input2, expected):
+    result = generate_diff(input1, input2)
+    correct = open(expected)
     assert result == correct.read()
