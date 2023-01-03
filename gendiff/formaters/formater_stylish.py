07fe67c444c76940d4f8b3668cb78d9d80b80da0
KarinA <abdullaeva.karina.r@gmail.com>
2023-01-04 00:41:12 +0300

    Refactor formater_stylish.py:
    1. Rename function 'get_normal_value' into 'to_str'.
    2. Edit functions 'to_str' and 'walk' to separate checks from formating.

--- gendiff/formaters/formater_stylish.py
+++ gendiff/formaters/formater_stylish.py
@@ -6,18 +6,17 @@ DEL = '  - '  # indent for DELeted elements
 SP = '    '   # indent only from SPaces for unchanged elements
 
 
-def get_normal_value(value):
+def to_str(value, level):
+    if isinstance(value, dict):
+        return walk(value, level)
     if isinstance(value, bool):
         return str(value).lower()
-    elif value is None:
+    if value is None:
         return 'null'
     return str(value)
 
 
 def walk(diff_dict, level):
-    if not isinstance(diff_dict, dict):
-        return get_normal_value(diff_dict)
-
     current_indent = SP * level
     lines = []
 
@@ -27,23 +26,23 @@ def walk(diff_dict, level):
 
         if status == 'added':
             lines.append(
-                f'{current_indent}{ADD}{key}: {walk(value, level + 1)}'
+                f'{current_indent}{ADD}{key}: {to_str(value, level + 1)}'
             )
         elif status == 'deleted':
             lines.append(
-                f'{current_indent}{DEL}{key}: {walk(value, level + 1)}'
+                f'{current_indent}{DEL}{key}: {to_str(value, level + 1)}'
             )
         elif status == 'updated':
             value1, value2 = value
             lines.append(
-                f'{current_indent}{DEL}{key}: {walk(value1, level + 1)}'
+                f'{current_indent}{DEL}{key}: {to_str(value1, level + 1)}'
             )
             lines.append(
-                f'{current_indent}{ADD}{key}: {walk(value2, level + 1)}'
+                f'{current_indent}{ADD}{key}: {to_str(value2, level + 1)}'
             )
         else:
             lines.append(
-                f'{current_indent}{SP}{key}: {walk(value, level + 1)}'
+                f'{current_indent}{SP}{key}: {to_str(value, level + 1)}'
             )
 
     result = itertools.chain("{", lines, [current_indent + "}"])
