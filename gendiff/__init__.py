1178ef76d0c59a697c3b10f20243589a5988356d
KarinA <abdullaeva.karina.r@gmail.com>
2023-01-05 22:57:41 +0300

    Refactor gendiff/__init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -8,16 +8,26 @@ from gendiff.difference import get_dict_of_differences
 from gendiff.formaters import design
 
 
-def parse_file(file_path):
-    with open(f'{file_path}') as input:
-        if file_path.endswith('json'):
-            return json.load(input)
-        else:
-            return yaml.safe_load(input)
+def parse(content, format_name):
+    if format_name == 'json':
+        return json.load(content)
+    if format_name == 'yaml':
+        return yaml.safe_load(content)
 
 
 def generate_diff(file_path1, file_path2, format='stylish'):
-    file1, file2 = map(parse_file, (file_path1, file_path2))
+    dicts = []
+
+    for file_path in file_path1, file_path2:
+        if file_path.endswith('.json'):
+            with open(f'{file_path}') as input:
+                file = parse(input, 'json')
+        elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
+            with open(f'{file_path}') as input:
+                file = parse(input, 'yaml')
+        dicts.append(file)
+
+    file1, file2 = dicts
     difference_dictionary = get_dict_of_differences(file1, file2)
 
     return design(difference_dictionary, format)
