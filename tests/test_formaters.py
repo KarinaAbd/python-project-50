a82d61a9730baae26a232c3e72ebcd6c627f22a1
KarinA <abdullaeva.karina.r@gmail.com>
2023-01-08 23:56:22 +0300

    Add fixture and test for better Test coverage

--- tests/test_formaters.py
+++ tests/test_formaters.py
@@ -7,8 +7,12 @@ from tests import get_full_path
 
 
 @pytest.mark.parametrize('input1, input2, format, expected', [
-    ('file1_nested.json', 'file2_nested.json', 'plain', 'plain_result.txt'),
-    ('file1_nested.yml', 'file2_nested.yml', 'plain', 'plain_result.txt')
+    ('file1_nested.json', 'file2_nested.json',
+        'plain', 'plain_result_nested.txt'),
+    ('file1_nested.yml', 'file2_nested.yml',
+        'plain', 'plain_result_nested.txt'),
+    ('file1.json', 'file2.json', 'plain', 'plain_result.txt'),
+    ('file1.yml', 'file2.yml', 'plain', 'plain_result.txt')
 ])
 def test_generate_diff_in_plain_format(input1, input2, format, expected):
     file1, file2 = map(get_full_path, (input1, input2))
@@ -32,7 +36,7 @@ def test_generate_diff_in_json_format(input1, input2, format):
     ('file1_nested.json', 'file2_nested.json', 'pain'),
     ('file1_nested.yml', 'file2_nested.yml', 'JSON')
 ])
-def test_generate_diff_in_wrong_format(input1, input2, format):
+def test_exception_in_generate_diff_in_wrong_format(input1, input2, format):
     file1, file2 = map(get_full_path, (input1, input2))
     with pytest.raises(Exception) as e:
         generate_diff(file1, file2, format)
